9710efb5e1e67081be053a84edb6cbd642634967
Nityanand Singh <nns.nitya@gmail.com>
2026-05-16 11:07:05 +0530

    Initial commit

---
+++ src/main/java/com/nns/blog/MyBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.nns.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nns/blog/MyBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nns.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

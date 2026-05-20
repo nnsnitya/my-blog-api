79ed44e30c626b31352aff21e0eb9a71e238d3c6
Nityanand Singh <nns.nitya@gmail.com>
2026-05-20 11:44:50 +0530

    validation added with exception
    devtools added

--- src/main/java/com/nns/blog/dto/common/UserDto.java
+++ src/main/java/com/nns/blog/dto/common/UserDto.java
@@ -1,9 +1,18 @@
 package com.nns.blog.dto.common;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public record UserDto(
       Long id,
+      @NotEmpty
+      @Size(min = 4, message = "Username must be min of 4 characters!!")
       String name,
+      @Email
       String email,
+      @NotEmpty
+      @Size(min = 3, max = 10, message = "Password must be min of 3 chars and max of 10 chars!!")
       String password,
       String about
 ) {
--- src/main/java/com/nns/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/nns/blog/exceptions/GlobalExceptionHandler.java
@@ -4,8 +4,13 @@ import com.nns.blog.dto.responses.Code;
 import com.nns.blog.dto.responses.ResponseHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +19,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> resoureceNotFoundExceptionHandler(ResourceNotFoundException ex) {
         String msg = ex.getMessage();
         return ResponseHandler.generateResp(msg, HttpStatus.NOT_FOUND, null, Code.FAILED.getCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgsNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> resp = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            resp.put(fieldName, message);
+        });
+        return ResponseHandler.generateResp("MethodArgsNotValidEx", HttpStatus.BAD_REQUEST, resp, Code.FAILED.getCode());
     }
 
 }

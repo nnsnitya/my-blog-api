c9f272b688b0a62f86b7277c33ecc99bfeea7b26
Nityanand Singh <nns.nitya@gmail.com>
2026-06-02 17:12:50 +0530

    Swagger Security: added
    Swagger UI / OpenAPI UI to support JWT authentication

--- src/main/java/com/nns/blog/MyBlogApiApplication.java
+++ src/main/java/com/nns/blog/MyBlogApiApplication.java
@@ -29,11 +29,11 @@ public class MyBlogApiApplication implements CommandLineRunner {
         try {
             Role role = new Role();
             role.setId(AppConstants.ADMIN_USER);
-            role.setName("ADMIN_USER");
+            role.setName("ROLE_ADMIN");
 
             Role role1 = new Role();
             role1.setId(AppConstants.NORMAL_USER);
-            role1.setName("NORMAL_USER");
+            role1.setName("ROLE_NORMAL");
 
             List<Role> roles = List.of(role, role1);
             List<Role> result = roleRepository.saveAll(roles);
--- src/main/java/com/nns/blog/config/SwaggerConfig.java
+++ src/main/java/com/nns/blog/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.nns.blog.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,7 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenApi() {
+        final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
                 .info(new Info()
@@ -20,6 +24,13 @@ public class SwaggerConfig {
                         .contact(new Contact()
                                 .name("Nityanand Singh")
                                 .email("nitya@gmail.com")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme().name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP).scheme("bearer")
+                                .bearerFormat("JWT")
                         )
                 );
     }
